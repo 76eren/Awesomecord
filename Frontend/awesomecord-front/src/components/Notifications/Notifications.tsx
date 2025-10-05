be9c10a4e4d0a928496243b480dfe404a04b1fa3
Eren <e.basaran2004@gmail.com>
2025-10-06 01:24:26 +0200

    fix: fixed toast not showing up

--- Frontend/awesomecord-front/src/components/Notifications/Notifications.tsx
+++ Frontend/awesomecord-front/src/components/Notifications/Notifications.tsx
@@ -3,7 +3,7 @@ import Navbar from "../Navbar/Navbar";
 import { useUserContext } from "../../lib/user-context";
 import { ensureHubStarted } from "../../realtime/signalrHub.ts";
 import type {UserModel} from "../../Models/User/user.model.ts";
-import {toast} from "react-toastify";
+import {toast, ToastContainer} from "react-toastify";
 
 export default function Notifications() {
     const { user, isLoading, error, fetchData, updateUser } = useUserContext();
@@ -38,13 +38,16 @@ export default function Notifications() {
     if (error != null) return <h1>An unknown error occured</h1>;
 
     return (
-        <div className="min-h-screen flex bg-gray-50">
-            <Navbar />
-            <main className="flex-1 p-6">
-                <h1 className="text-2xl font-semibold mb-4">Notifications</h1>
-                <p>You have currently {user.receivedFriendRequests.length} incoming requests</p>
-                <p>You have currently {user.sentFriendRequests.length} outgoing requests</p>
-            </main>
-        </div>
+        <>
+            <ToastContainer></ToastContainer>
+            <div className="min-h-screen flex bg-gray-50">
+                <Navbar />
+                <main className="flex-1 p-6">
+                    <h1 className="text-2xl font-semibold mb-4">Notifications</h1>
+                    <p>You have currently {user.receivedFriendRequests.length} incoming requests</p>
+                    <p>You have currently {user.sentFriendRequests.length} outgoing requests</p>
+                </main>
+            </div>
+        </>
     );
 }
